962fde0d0da674f849752ad038e01489832637f2
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-02 12:32:45 -0500

    Imported bootstrap & axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import CitySearch from "./CitySearch";
-import "./styles.css";
+import "./App.css";
 
 export default function App() {
   return (
